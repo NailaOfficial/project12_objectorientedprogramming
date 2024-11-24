7d5be900cbd1da65d3d655d2b5aaf473bcfe1e7b
NailaOfficial <nameerofficial@gmail.com>
2024-11-24 22:34:21 +0500

    project12_objectorientedprogramming

---
+++ index.ts
@@ -0,0 +1,79 @@
+import inquirer from "inquirer";
+class Book {
+    constructor(public title: string, public author: string) {}
+}
+
+class Library {
+    books: Book[] = [];
+
+    addBook(book: Book) {
+        this.books.push(book);
+        console.log(`Book "${book.title}" by ${book.author} added successfully!`);
+    }
+
+    listBooks() {
+        if (this.books.length === 0) {
+            console.log("No books in the library.");
+        } else {
+            console.log("Books in the library:");
+            this.books.forEach((book, index) => {
+                console.log(`${index + 1}. ${book.title} by ${book.author}`);
+            });
+        }
+    }
+
+    borrowBook(bookTitle: string) {
+        const bookIndex = this.books.findIndex(book => book.title === bookTitle);
+        if (bookIndex !== -1) {
+            const borrowedBook = this.books.splice(bookIndex, 1)[0];
+            console.log(`You borrowed "${borrowedBook.title}" by ${borrowedBook.author}`);
+        } else {
+            console.log(`The book "${bookTitle}" is not available in the library.`);
+        }
+    }
+}
+
+const library = new Library();
+
+async function main() {
+    while (true) {
+        const answer = await inquirer.prompt({
+            name: "action",
+            message: "What would you like to do?",
+            type: "list",
+            choices: ["Add Book", "List Books", "Borrow Book", "Exit"]
+        });
+
+        if (answer.action === "Add Book") {
+            const bookDetails = await inquirer.prompt([
+                { name: "title", message: "Enter the book title", type: "input" },
+                { name: "author", message: "Enter the book author", type: "input" }
+            ]);
+            const newBook = new Book(bookDetails.title, bookDetails.author);
+            library.addBook(newBook);
+
+            console.log("Current library after adding book:", library.books);
+        } else if (answer.action === "List Books") {
+        
+            console.log("Library state before listing books:", library.books);
+            
+            library.listBooks();
+        } else if (answer.action === "Borrow Book") {
+            if (library.books.length === 0) { 
+                console.log("No books available to borrow.");
+            } else {
+                const borrowAnswer = await inquirer.prompt({
+                    name: "bookTitle",
+                    message: "Enter the title of the book you want to borrow:",
+                    type: "input"
+                });
+                library.borrowBook(borrowAnswer.bookTitle);
+            }
+        } else if (answer.action === "Exit") {
+            console.log("Goodbye!");
+            break;
+        }
+    }
+}
+
+main();
